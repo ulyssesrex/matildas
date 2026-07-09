c59bb7e35e7f68873c431fafd9af5dfc5173bdad
Dan WB <7935931+ulyssesrex@users.noreply.github.com>
2026-07-09 16:39:05 -0400

    Move authentication to admin namespace

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Admin::SessionsController < ApplicationController
+  # Allows an unauthenticated visitor to see and submit the admin login form
+  allow_unauthenticated_access only: [ :new, :create ]
+  
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_admin_session_url, alert: "Try again later." }
+
+  def new
+    # Renders the native login form
+  end
+
+  def create
+    if user = User.authenticate_by(email_address: params[:email_address], password: params[:password])
+      # Verification step: ensure the authenticated user is an administrator
+      if user.admin?
+        start_new_session_for user
+        redirect_to admin_root_path, notice: "Logged in to Admin Dashboard."
+      else
+        redirect_to new_admin_session_path, alert: "Access denied: Unauthorized account."
+      end
+    else
+      redirect_to new_admin_session_path, alert: "Try again. Incorrect email or password."
+    end
+  end
+
+  def destroy
+    terminate_session
+    redirect_to new_admin_session_path, notice: "Logged out.", status: :see_other
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home"
+  
+  # config/routes.rb
+  namespace :admin do
+    root to: "pages#home"
+
+    resource :password, param: :token, only: [ :new, :create, :edit, :update ]
+    resource :session, only: [ :new, :create, :destroy ]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  root "pages#home"
 end
