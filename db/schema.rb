a92c83125c15b3fc684443678846d73023d916fe
Dan WB <7935931+ulyssesrex@users.noreply.github.com>
2026-07-09 11:28:36 -0400

    Create Admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_09_144251) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_09_152311) do
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.string "username", null: false
+    t.index ["username"], name: "index_admins_on_username", unique: true
+  end
+
   create_table "links", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
