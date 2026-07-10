d6e1e39829ff46c16a3fdeca8626faa78a45520f
Dan Wichman-Buescher <danielwb@meraki.com>
2026-07-10 11:47:31 -0700

    Add top nav bar

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /" do
+    it "renders navigation links to each home page section" do
+      get root_path
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include('<nav class="site-nav" aria-label="Primary navigation">')
+      expect(response.body).to include('<a class="site-nav__link site-nav__brand" href="#top">THE MOON RINGERS</a>')
+      expect(response.body).to include('<a class="site-nav__link" href="#music">Music</a>')
+      expect(response.body).to include('<a class="site-nav__link" href="#shows">Shows</a>')
+      expect(response.body).to include('<a class="site-nav__link" href="#misc">Misc</a>')
+      expect(response.body).to include('<main id="top"')
+      expect(response.body).to include('<section id="music"')
+      expect(response.body).to include('<section id="shows"')
+      expect(response.body).to include('<section id="misc"')
+    end
   end
 end
